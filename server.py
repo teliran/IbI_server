ce2ffac830cb4fb702094148fcfc2839021fd0b9
teliran <teliran@gmail.com>
2019-03-29 12:01:27 +0300

    function for add users and ratings

--- server.py
+++ server.py
@@ -9,19 +9,19 @@ app = Flask(__name__)
 app.config.from_pyfile('server.cfg', silent=True)
 db = SQLAlchemy(app)
 
-class Useres(db.Model):
-    user_id = db.Column(db.Integer, primary_key = True)
-    datecreated = db.Column(db.DateTime)
+class Users(db.Model):
+    user_id = db.Column(db.String, primary_key = True)
+    date_created = db.Column(db.String)
     age = db.Column(db.Integer)
     sex = db.Column(db.Text)
 
 class ImageRatings(db.Model):
-    user_id = db.Column(db.Integer, primary_key = True)
-    imageid = db.Column(db.Text, primary_key = True)
+    user_id = db.Column(db.String, primary_key = True)
+    image_id = db.Column(db.Text, primary_key = True)
     rating = db.Column(db.Integer)
 
 class Actions(db.Model):
-    user_id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.String, primary_key = True)
     session_id = db.Column(db.Integer, primary_key = True)
     timestamp = db.Column(db.DateTime, primary_key = True)
     total_screens = db.Column(db.Integer)
@@ -32,13 +32,28 @@ class Actions(db.Model):
     shown_images = db.Column(db.String)
     top_rated_images = db.Column(db.String)
 
-
-
-
 @app.route('/healthcheck', methods=['GET'])
 def printer():
     print("called")
     return json.dumps('Server is listening...')
+
+@app.route('/ratings', methods=['POST'])
+def add_ratings():
+    print("add_ratings")
+    print("json:", request.json)
+    data = ImageRatings(user_id=request.get_json(force=True)["userId"], image_id=request.get_json(force=True)["imageId"], rating=request.get_json(force=True)["rating"])
+    db.session.add(data)
+    db.session.commit()
+    return json.dumps('Rating Added')
+
+@app.route('/users', methods=['POST'])
+def add_users():
+    print("add_users")
+    print("json:", request.json)
+    data = Users(user_id=request.get_json(force=True)["userId"], date_created=request.get_json(force=True)["date"], age=request.get_json(force=True)["age"], sex=request.get_json(force=True)["sex"])
+    db.session.add(data)
+    db.session.commit()
+    return json.dumps('User Added')
 
 if __name__ == '__main__':
     db.create_all()
